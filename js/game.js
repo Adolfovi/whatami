97b1412e6714506ebe450535cb5257bb316b797f
Adolfovi <32370254+Adolfovi@users.noreply.github.com>
2021-06-29 11:30:10 +0200

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,37 @@
+const elephant = new Audio("audio/elephant.mp3");
+const dog = new Audio("audio/dog.mp3");
+const cat = new Audio("audio/cat.mp3");
+const rat = new Audio("audio/rat.mp3");
+const lion = new Audio("audio/lion.mp3");
+
+
+
+function imanelephant(){
+alert("I'M AN ELEPHANT!!");
+elephant.play();
+elephant.loop = false;
+}
+
+function imadog(){
+    alert("I'M A DOG!!");
+    dog.play();
+    dog.loop = false; 
+}
+
+function imacat(){
+    alert("I'M A CAT!!");
+    cat.play();
+    cat.loop = false;
+}
+
+function imarat(){
+    alert("I'M A RAT!!");
+    rat.play();
+    rat.loop = false;
+}
+
+function imalion(){
+    alert("I'M A LION!!");
+    lion.play();
+    lion.loop = false;
+}
